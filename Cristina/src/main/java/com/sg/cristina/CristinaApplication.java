c546f35337664640efea0f2be1fecabedcf7c2c5
jiangwei.wh@outlook.com <jiangwei.wh@outlook.com>
2019-04-19 11:22:52 +0800

    init

---
+++ Cristina/src/main/java/com/sg/cristina/CristinaApplication.java
@@ -0,0 +1,13 @@
+package com.sg.cristina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CristinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CristinaApplication.class, args);
+	}
+
+}
